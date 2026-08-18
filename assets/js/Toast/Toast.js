2410fa4e3e94068f3869d6f291a83df0446c2900
Sophia M <sophia.moerchen@gmail.com>
2026-08-18 13:46:34 +0200

    feat: delete toast & renamed new-toast as toast to keep log on file

--- assets/js/Toast/Toast.js
+++ assets/js/Toast/Toast.js
@@ -1,19 +1,67 @@
-class AddToast {
+class Toast {
     constructor() {
         this.template = `
-        <div class="toast">
             <div class="message"></div>
             <div class="close" data-close-div>X</div>
-        </div>
         `
     }
-    displayToasts() {
-    document.querySelectorAll('.toast').forEach((element) => {
-        element.style.opacity = 1
+    displayAll() {
+        document.querySelectorAll('.toast').forEach((element) => {
+            element.style.opacity = 1
 
-        // addEventCloseToast(element, element.querySelector('.close'))
-        // closeToast(element)
-    })
-}
+            this.addEventCloseToast(element, element.querySelector('.close'))
+            this.closeToast(element)
+        })
+    }
+    displayToast(type, message) {
+        this.closeToasts()
+
+        const toastDiv = this.createToastDivs(type, message)
+
+        // display toast
+        setTimeout((e) => {
+            toastDiv.style.opacity = 1
+        }, 100)
+
+        // remove toast
+        this.closeToast(toastDiv)
+    }
+    createToastDivs(type, message) {
+        const toastDiv = document.createElement('div')
+        toastDiv.classList.add('toast')
+        toastDiv.classList.add(type)
+        toastDiv.innerHTML = this.template
+
+        toastDiv.querySelector('.message').innerText = message        
+
+        this.addEventCloseToast(toastDiv, toastDiv.querySelector('.close'))
+        
+        document.body.append(toastDiv)
+
+        return toastDiv
+    }
+
+    closeToast(toastDiv, fromEvent = false) {
+
+        let opacityTime = 8000
+        let removeTime = 9000
+        if (fromEvent === true) {
+            opacityTime = 0
+            removeTime = 100
+        }
+        setTimeout((event) => {
+            toastDiv.style.removeProperty('opacity')
+        }, opacityTime );
+
+        setTimeout((event) => {
+            toastDiv.remove()
+        }, removeTime);
+    }
+
+    addEventCloseToast(toastDiv, toastCloseDiv) {
+        toastCloseDiv.addEventListener('click', (event) => {
+        this.closeToast(toastDiv, true)
+        })
+    }
 
 }
